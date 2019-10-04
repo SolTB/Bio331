4d26219e61417bd6dbf8691ae12544415381ac88
Sol Taylor-Brill <39569127+SolTB@users.noreply.github.com>
2019-10-03 23:39:50 -0700

    HW3 update 10/03/19 23:39

--- HW3_NEW.py
+++ HW3_NEW.py
@@ -5,20 +5,26 @@ Date: 10/03/19
 '''
 
 import matplotlib.pyplot as plt
+import random
 
 def main():
-	foutput = FileReader("test-edges1.txt")
+	foutput = FileReader("EGFR1-edges.txt")
 	nodes = foutput[0]
 	edges = foutput[1]
-	s="A" #the starting node
-	T = 100
-	q = 0.5
+
+	s="EGF" #the starting node
+	Tprob = 1000
+	Tsim = 100000
+	q = 0.95
+
 	Gin, Gout = Neighbor_Dicts(nodes,edges)
 	G_in, G_out = add_edges(Gin, Gout, nodes,edges,s)
-	X = rw_probs(nodes,G_in,G_out,s,T,q)
+	X = rw_probs(nodes,G_in,G_out,s,Tprob,q)
+	TimeDict = rw_simulate(nodes,G_out,s,Tsim,q)
 	return
 
 #This function just reads the edgefile. It's based on a function from HW2 but it uses lists instead of sets
+#and doesn't include the edge type information
 def FileReader(f):
 	print("reading " + f)
 	EdgeList = open(f,'r')
@@ -29,11 +35,15 @@ def FileReader(f):
 	for l in EdgeList:
 		line = str(l)
 		nodesinline = line.split()
+		n1 = nodesinline[0]
+		n2 = nodesinline[1]
+		file_edge = [n1,n2] #doesn't include edge type
 		if nodesinline not in edges:
 			edges.append(nodesinline)
-		for n in nodesinline:
-			if n not in nodes:
-				nodes.append(n)
+		if n1 not in nodes:
+			nodes.append(n1)
+		if n2 not in nodes:
+			nodes.append(n2)
 	return nodes, edges
 
 #makes Gin and Gout (in neighbors and out neighbors) dictionaries
@@ -98,7 +108,30 @@ def rw_probs(nodes,Gin,Gout,s,T,q):
 		table_list.append(tlist) #making a list of lists
 	return table_list
 
-def rw_simulate():
-	return
+#This function simulates a random walker moving through a graph and 
+#outputs a dictionary of times a node was visited by a random walker
+#inputs = nodes, G_out (dictionary node->Nout), s (source node), T (time steps), q (probability [0-1])
+def rw_simulate(nodes,G_out,s,T,q):
+	TimeDict = {} #Dictionary node->times walker visited
+	for n in nodes: #initializing TimeDict w/ all nodes except s at 0
+		if n == s:
+			TimeDict[n] = 1 #because it starts at 1
+		else:
+			TimeDict[n] = 0 #all other nodes initialized at 0
+
+	#Moving through time steps
+	CurrentNode = s #where the traveler is at. 
+	for t in range(1,T):
+		start = CurrentNode
+		n_out = G_out[start] #a list of all outgoing nodes of the current node
+		num = random.random() #generates random number between 0 and 1
+		if num > q: 
+			CurrentNode = s #redefining the current node. Teleport back to source
+		else:
+			newnode = random.choice(n_out) #choose random outgoing neighbor as new current node
+			CurrentNode = newnode
+		numtimes = TimeDict[CurrentNode] + 1 #adds 1 to the node that was just moved to
+		TimeDict[CurrentNode] = numtimes #redefines node that just got moved to
+	return TimeDict
 
 main()
